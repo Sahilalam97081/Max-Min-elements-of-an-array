5a7b9393e81cb592921e9005163851e893819345
Sahil Alam <160860074@ggi.ac.in>
2025-10-27 23:27:31 +0530

    Add files via upload

---
+++ min array.cpp
@@ -0,0 +1,28 @@
+
+/*
+// find the max and min elements of array 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of an array:";
+    cin>>n;
+    int arr[n];
+    cout<<"The array elements are:";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int max=arr[0];
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+        else if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    cout<<"The maximum value is:"<<max<<endl;
+    cout<<"The minimum value is:"<<min<<endl;
+    return 0;
+}  */
